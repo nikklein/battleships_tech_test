822b7a0f9dac54e8197940ea81df2e5c6e8f84e1
nikklein <objcoding@gmail.com>
2016-09-20 15:52:17 +0100

    Add test for place ship method

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -13,4 +13,16 @@ describe("Board", function() {
       expect(board.board[i].length).toEqual(10);
     }
   })
+  it("can place ship horizontally on the board", function() {
+    var ship = [1,1,1];
+    board.place_ship(0,0,0,0, ship);
+    expect(board.board[0]).toEqual([1,1,1,0,0,0,0,0,0,0])
+  })
+  it("can place ship vertically on the board", function() {
+    var ship = [1,1,1];
+    board.place_ship(0,0,0,3, ship);
+    expect(board.board[0]).toEqual([1,0,0,0,0,0,0,0,0,0])
+    expect(board.board[0]).toEqual([1,0,0,0,0,0,0,0,0,0])
+    expect(board.board[0]).toEqual([1,0,0,0,0,0,0,0,0,0])
+  })
 });
