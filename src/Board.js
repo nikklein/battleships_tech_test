e9c4b7b9678bb84ccdda67990570ad7f7e2a69fa
nikklein <objcoding@gmail.com>
2016-09-20 15:27:33 +0100

    Add board, place ship method

---
+++ src/Board.js
@@ -0,0 +1,55 @@
+function Board() {
+  this.board =
+  [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+]}
+
+Board.prototype.place_ship = function(a,b,c,d,ship){
+  if(b == d){
+    this.board[b].splice.apply(this.board[b], [a, ship.length].concat(ship));
+  } else {
+      for(var i = b; i < d;i++){
+        this.board[i].splice(a, 1, ship[0]);
+    }
+  }
+}
+var ship1 = [1,1];
+var ship2 = [2,2,2];
+var ship3 = [3,3,3,3];
+
+// var keys_map = {
+//   1:"A",2:"B",3:"C",4:"D",5:"E",
+//   6:"F",7:"G",8:"H",9:"I",10:"G"
+//  }
+
+
+ // function createTable(tableData) {
+ //   var table = document.createElement('table');
+ //   var tableBody = document.createElement('tbody');
+ //
+ //   tableData.forEach(function(rowData) {
+ //     var row = document.createElement('tr');
+ //
+ //     rowData.forEach(function(cellData) {
+ //       var cell = document.createElement('td');
+ //       cell.appendChild(document.createTextNode(cellData));
+ //       row.appendChild(cell);
+ //     });
+ //
+ //     tableBody.appendChild(row);
+ //   });
+ //
+ //   table.appendChild(tableBody);
+ //   document.body.appendChild(table);
+ // }
+ //
+ // createTable(board);
