414a346cb9be4b4810fd8fff21ee6c20a309831a
nikklein <objcoding@gmail.com>
2016-09-20 16:03:29 +0100

    Add test

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -13,16 +13,24 @@ describe("Board", function() {
       expect(board.board[i].length).toEqual(10);
     }
   })
-  it("can place ship horizontally on the board", function() {
+  it("can place a ship horizontally on the board", function() {
     var ship = [1,1,1];
     board.placeShip(0,0,0,0, ship);
     expect(board.board[0]).toEqual([1,1,1,0,0,0,0,0,0,0])
   })
-  it("can place ship vertically on the board", function() {
+  it("can place a ship vertically on the board", function() {
     var ship = [1,1,1];
     board.placeShip(0,0,0,3, ship);
     expect(board.board[0]).toEqual([1,0,0,0,0,0,0,0,0,0])
     expect(board.board[0]).toEqual([1,0,0,0,0,0,0,0,0,0])
     expect(board.board[0]).toEqual([1,0,0,0,0,0,0,0,0,0])
   })
+  it("should throw an exception if one ship overlaps another one", function() {
+    var ship = [1,1,1];
+    var ship2 = [2,2,2];
+    board.placeShip(0,0,3,0, ship);
+        expect(function() {
+        board.placeShip(0,0,3,0, ship2);
+      }).toThrowError("Can't place a ship");
+    });
 });
